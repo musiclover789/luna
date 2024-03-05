9ea266742806560c6e335f97087a64f9bd56d8fe
musiclover789 <129953723+musiclover789@users.noreply.github.com>
2024-03-06 00:50:05 +0800

    Update dom.go

--- base_devtools/dom/dom.go
+++ base_devtools/dom/dom.go
@@ -39,6 +39,37 @@ func GetDocument(conn *protocol.DevToolsConn, depth int, pierce bool) gjson.Resu
 	return gjson.ParseBytes(luna_utils.FormatJSONAsBytes(<-ch))
 }
 
+func QuerySelector(conn *protocol.DevToolsConn, nodeId int64, selector string) gjson.Result {
+	id := luna_utils.IdGen.NextID()
+	req := map[string]interface{}{
+		"id":     id,
+		"method": "DOM.querySelector",
+		"params": map[string]interface{}{
+			"nodeId":   nodeId,
+			"selector": selector,
+		},
+	}
+	ch := make(chan map[string]interface{})
+	conn.SubscribeOneTimeEvent(strconv.Itoa(id), func(param interface{}) {
+		ch <- param.(map[string]interface{})
+	})
+	conn.WriteMessage(req)
+	return gjson.ParseBytes(luna_utils.FormatJSONAsBytes(<-ch))
+}
+
+func SetFileInputFiles(conn *protocol.DevToolsConn, nodeId int64, files []string) {
+	id := luna_utils.IdGen.NextID()
+	req := map[string]interface{}{
+		"id":     id,
+		"method": "DOM.setFileInputFiles",
+		"params": map[string]interface{}{
+			"files":  files,
+			"nodeId": nodeId,
+		},
+	}
+	conn.WriteMessage(req)
+}
+
 func GetOuterHTML(conn *protocol.DevToolsConn, nodeID int64) gjson.Result {
 	id := luna_utils.IdGen.NextID()
 	req := map[string]interface{}{
@@ -73,14 +104,14 @@ func GetOuterHTMLWithMap(conn *protocol.DevToolsConn, nodeID int64) map[string]i
 	return <-ch
 }
 
-func SetOuterHTML(conn *protocol.DevToolsConn, nodeID int64,outerHTML string) gjson.Result {
+func SetOuterHTML(conn *protocol.DevToolsConn, nodeID int64, outerHTML string) gjson.Result {
 	id := luna_utils.IdGen.NextID()
 	req := map[string]interface{}{
 		"id":     id,
 		"method": "DOM.setOuterHTML",
 		"params": map[string]interface{}{
-			"nodeId": nodeID,
-			"outerHTML":outerHTML,
+			"nodeId":    nodeID,
+			"outerHTML": outerHTML,
 		},
 	}
 	ch := make(chan map[string]interface{})
