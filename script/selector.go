40d7e8d72a6284dbe095d754a95e74ebce35f00e
musiclover789 <129953723+musiclover789@users.noreply.github.com>
2024-03-08 20:40:00 +0800

    Update selector.go

--- script/selector.go
+++ script/selector.go
@@ -1,44 +1,89 @@
 package script
 
 func GetElementPositionByCss(selector string) string {
+	//return `
+	//	function getElementPosition(selector) {
+	//	  var element = document.querySelector(selector);
+	//	  if (element) {
+	//		var rect = element.getBoundingClientRect();
+	//		return {
+	//		  ok: true,
+	//		  top: rect.top + window.scrollY,
+	//		  left: rect.left + window.scrollX,
+	//		  width: rect.width,
+	//		  height: rect.height
+	//		};
+	//	  } else {
+	//		return { ok: false};
+	//	  }
+	//	}
+	//	getElementPosition("` + selector + `");
+	//`
+
 	return `
-		function getElementPosition(selector) {
-		  var element = document.querySelector(selector);
-		  if (element) {
-			var rect = element.getBoundingClientRect();
-			return {
-			  ok: true,
-			  top: rect.top + window.scrollY,
-			  left: rect.left + window.scrollX,
-			  width: rect.width,
-			  height: rect.height
-			};
-		  } else {
-			return { ok: false};
-		  }
-		}	
-		getElementPosition("` + selector + `");
-	`
+	function getElementViewportCoordinates(selector) {
+    const element = document.querySelector(selector);
+    
+    if (!element) {
+        return { ok: false};
+    }
+    const rect = element.getBoundingClientRect();
+    return {
+ 		ok: true,
+        top: rect.top,
+        left: rect.left,
+        width: rect.width,
+        height: rect.height
+    };
+   }
+
+   getElementViewportCoordinates("` + selector + `");
+
+ `
 }
 
 func GetElementPositionByXpath(selector string) string {
+	//return `
+	//	function getElementPositionByXPath(xpath) {
+	//	  var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+	//	  if (element) {
+	//		var rect = element.getBoundingClientRect();
+	//		return {
+	//	      ok: true,
+	//		  top: rect.top + window.scrollY,
+	//		  left: rect.left + window.scrollX,
+	//		  width: rect.width,
+	//		  height: rect.height
+	//		};
+	//	  } else {
+	//		return { ok: false};
+	//	  }
+	//	}
+	//	getElementPositionByXPath(` + "`" + selector + "`" + `);
+	//
+	//`
 	return `
-		function getElementPositionByXPath(xpath) {
-		  var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-		  if (element) {
-			var rect = element.getBoundingClientRect();
-			return {
-		      ok: true,
-			  top: rect.top + window.scrollY,
-			  left: rect.left + window.scrollX,
-			  width: rect.width,
-			  height: rect.height
-			};
-		  } else {
+	function getElementByXpath(path) {
+		return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+	}
+	
+	function getElementViewportCoordinatesByXpath(xpath) {
+		const element = getElementByXpath(xpath);
+		
+		if (!element) {
 			return { ok: false};
-		  }
 		}
-		getElementPositionByXPath(` + "`" + selector + "`" + `);
-
+	
+		const rect = element.getBoundingClientRect();
+	
+		return {
+ 			ok: true,
+			top: rect.top,
+			left: rect.left,
+			width: rect.width,
+			height: rect.height
+		};
+	}
+	getElementViewportCoordinatesByXpath(` + "`" + selector + "`" + `);
 	`
 }
