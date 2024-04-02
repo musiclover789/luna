a70e18eca1052a6dc310e9036d27a6b5a84cbce1
musiclover789 <129953723+musiclover789@users.noreply.github.com>
2024-04-03 01:57:00 +0800

    Update reverse_proxy.go
    优化代理

--- reverse_proxy/reverse_proxy.go
+++ reverse_proxy/reverse_proxy.go
@@ -10,24 +10,56 @@ import (
 	"net"
 	"net/http"
 	"strconv"
-	"time"
+	"sync"
 )
 
-func StartProxyServer(proxyType, host, port, user, password string) (string, error) {
+// ProxyServer 结构体包含代理服务器相关的信息和操作
+type ProxyServer struct {
+	server       *goproxy.ProxyHttpServer
+	listener     net.Listener
+	proxyType    string
+	host         string
+	port         string
+	user         string
+	password     string
+	listenerLock sync.Mutex
+}
+
+// NewProxyServer 创建一个新的代理服务器实例
+func NewProxyServer(proxyType, host, port, user, password string) *ProxyServer {
+	return &ProxyServer{
+		server:    goproxy.NewProxyHttpServer(),
+		proxyType: proxyType,
+		host:      host,
+		port:      port,
+		user:      user,
+		password:  password,
+	}
+}
+
+// Start 启动代理服务器
+func (p *ProxyServer) Start() (string, error) {
+	p.listenerLock.Lock()
+	defer p.listenerLock.Unlock()
+
+	if p.listener != nil {
+		return "", errors.New("proxy server is already running")
+	}
+
 	// 设置代理IP
 	var auth *proxy.Auth
-	if user != "" && password != "" {
+	if p.user != "" && p.password != "" {
 		auth = &proxy.Auth{
-			User:     user,
-			Password: password,
+			User:     p.user,
+			Password: p.password,
 		}
 	}
 
 	var dialFunc func(string, string) (net.Conn, error)
 
-	switch proxyType {
+	switch p.proxyType {
 	case "http":
-		proxyURL := fmt.Sprintf("http://%s:%s@%s:%s", user, password, host, port)
+		proxyURL := fmt.Sprintf("http://%s:%s@%s:%s", p.user, p.password, p.host, p.port)
 		dialer, err := proxyclient.NewProxyClient(proxyURL)
 		if err != nil {
 			return "", fmt.Errorf("failed to create proxy dialer: %w", err)
@@ -36,7 +68,7 @@ func StartProxyServer(proxyType, host, port, user, password string) (string, err
 			return dialer.Dial(network, addr)
 		}
 	case "https":
-		proxyURL := fmt.Sprintf("https://%s:%s@%s:%s", user, password, host, port)
+		proxyURL := fmt.Sprintf("https://%s:%s@%s:%s", p.user, p.password, p.host, p.port)
 		dialer, err := proxyclient.NewProxyClient(proxyURL)
 		if err != nil {
 			return "", fmt.Errorf("failed to create proxy dialer: %w", err)
@@ -45,7 +77,7 @@ func StartProxyServer(proxyType, host, port, user, password string) (string, err
 			return dialer.Dial(network, addr)
 		}
 	case "socks5":
-		dialer, err := proxy.SOCKS5("tcp", net.JoinHostPort(host, port), auth, proxy.Direct)
+		dialer, err := proxy.SOCKS5("tcp", net.JoinHostPort(p.host, p.port), auth, proxy.Direct)
 		if err != nil {
 			return "", fmt.Errorf("failed to create proxy dialer: %w", err)
 		}
@@ -58,34 +90,46 @@ func StartProxyServer(proxyType, host, port, user, password string) (string, err
 
 	// 创建自定义的 Transport
 	tr := &http.Transport{
-		Dial: dialFunc,
+		Dial:                dialFunc,
+		MaxIdleConnsPerHost: 100, // 连接池大小
 	}
 
-	// 创建 ProxyServer
-	proxyServer := goproxy.NewProxyHttpServer()
-	proxyServer.Tr = tr
+	// 设置 ProxyServer 的 Transport
+	p.server.Tr = tr
 
 	// 获取一个随机端口
-	l, err := net.Listen("tcp", ":0")
+	var err error
+	p.listener, err = net.Listen("tcp", ":0")
 	if err != nil {
 		return "", fmt.Errorf("failed to listen on a random port: %w", err)
 	}
-	port = strconv.Itoa(l.Addr().(*net.TCPAddr).Port)
+	p.port = strconv.Itoa(p.listener.Addr().(*net.TCPAddr).Port)
 
 	// 启动 ProxyServer
-	ch := make(chan error)
 	go func() {
-		log.Printf("Proxy server is listening on :%s", port)
-		ch <- http.Serve(l, proxyServer)
+		log.Printf("Proxy server is listening on :%s", p.port)
+		err := http.Serve(p.listener, p.server)
+		if err != nil && err != http.ErrServerClosed {
+			log.Printf("Proxy server error: %v", err)
+		}
 	}()
 
-	// 等待代理服务器启动完成
-	select {
-	case err := <-ch:
-		return "", err
-	case <-time.After(time.Second):
-	}
-
-	return port, nil
+	return p.port, nil
 }
 
+// Stop 停止代理服务器
+func (p *ProxyServer) Stop() error {
+	p.listenerLock.Lock()
+	defer p.listenerLock.Unlock()
+
+	if p.listener != nil {
+		err := p.listener.Close()
+		if err != nil {
+			return err
+		}
+		log.Println("Proxy server stopped")
+		p.listener = nil
+		return nil
+	}
+	return errors.New("proxy server is not running")
+}
