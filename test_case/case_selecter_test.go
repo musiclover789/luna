b83020ca5fc40353e2e0704877157f6ce26ff114
musiclover789 <129953723+musiclover789@users.noreply.github.com>
2024-04-08 02:44:22 +0800

    Update case_selecter_test.go

--- test_case/case_selecter_test.go
+++ test_case/case_selecter_test.go
@@ -15,7 +15,7 @@ func TestSelector(t *testing.T) {
 	luna_utils.KillProcess()
 	/********************************/
 	chromiumPath := "/Users/hongyuji/Documents/workspace/golang/Chromium.app/Contents/MacOS/Chromium"
-	browserObj := devtools.NewBrowser(chromiumPath, &devtools.BrowserOptions{
+	_, browserObj := devtools.NewBrowser(chromiumPath, &devtools.BrowserOptions{
 		CachePath: luna_utils.CreateCacheDirInSubDir("/Users/hongyuji/Documents/workspace/golang/cache"),
 		Headless:  false,
 	})
